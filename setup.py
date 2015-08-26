29b9d7c17586bfbf5902569ad34c8f1f24ae5efc
Dan Drake <dr.dan.drake@gmail.com>
2015-08-26 16:27:58 -0700

    version 3.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(name='sagetex',
       long_description="""The SageTeX package allows you to embed code,
   results of computations, and plots from the Sage mathematics
   software suite (http://sagemath.org) into LaTeX documents.""",
-      version='2.3.4',
+      version='3.0',
       author='Dan Drake',
       author_email='dr.dan.drake@gmail.com',
       url='https://github.com/dandrake/sagetex',
