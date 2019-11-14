2a8f3e12a97a353f2e3d9372b9da6e1f346d55f8
Dima Pasechnik <dimpase@gmail.com>
2019-11-14 11:02:09 +0000

    bump up version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(name='sagetex',
       long_description="""The SageTeX package allows you to embed code,
   results of computations, and plots from the Sage mathematics
   software suite (http://sagemath.org) into LaTeX documents.""",
-      version='3.3',
+      version='3.4',
       author='Dan Drake',
       author_email='dr.dan.drake@gmail.com',
       maintainer='SageMath developers',
