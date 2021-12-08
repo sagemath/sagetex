a2625cac1c86b969e05b2ebb980ff35f106f768c
Dima Pasechnik <dima@pasechnik.info>
2021-12-08 23:00:51 +0000

    switching to setuptools

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
-from distutils.core import setup
-setup(name='sagetex',
+from setuptools import setup, find_packages
+
+setup(
+      name='sagetex',
       description='Embed Sage code and plots into LaTeX',
       long_description="""The SageTeX package allows you to embed code,
   results of computations, and plots from the Sage mathematics
   software suite (http://sagemath.org) into LaTeX documents.""",
+      long_description_content_type='text/markdown',
       version='3.6',
       author='Dan Drake',
       author_email='dr.dan.drake@gmail.com',
@@ -12,8 +15,14 @@ setup(name='sagetex',
       url='https://github.com/sagemath/sagetex',
       license='GPLv2+',
       py_modules=['sagetex', 'sagetexparse'],
+      packages=find_packages(),
       scripts=['sagetex-run', 'sagetex-extract', 'sagetex-makestatic', 'sagetex-remote'],
       install_requires=['pyparsing'],
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+        "Operating System :: OS Independent",
+      ],
       data_files = [('share/texmf/tex/latex/sagetex',
         ['example.tex',
          'CONTRIBUTORS',
